699170be9af6c6fa376d45403b2e356175848320
om vazire <omvazire1195@gmail.com>
2026-05-30 10:43:36 +0530

    cofiguration specific for f1

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,19 @@ const ai = new GoogleGenAI({apiKey: GEMINI_API_KEY});
 async function main() {
   const response = await ai.models.generateContent({
     model: 'gemini-2.5-flash',
-    // instructions: 'you are a f1 expert and only answer f1 realted questions'
-    contents: 'how goated is lewis hamilton',
+    config: {
+    systemInstruction: `
+      You are F1GPT.
+
+      You are a Formula 1 expert.
+
+      Answer only Formula 1 related questions.
+
+      If user asks unrelated questions,
+      politely tell them that you only discuss Formula 1.
+    `
+  },
+    contents: 'give me 2 line ans hamilton vs verstappen',
   });
   console.log(response.text);
 }
