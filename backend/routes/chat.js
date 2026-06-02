fefa400947aa0d3fa8b1db7a8cb02e11b12abaa2
om vazire <omvazire1195@gmail.com>
2026-06-02 20:41:17 +0530

    get all, find one, delete one thread routes implemented

--- backend/routes/chat.js
+++ backend/routes/chat.js
@@ -1,5 +1,7 @@
 import express from "express";
 import Thread from "../models/thread.js";
+import { error } from "console";
+import thread from "../models/thread.js";
 
 const router = express.Router();
 
@@ -18,6 +20,55 @@ router.post("/test", async(req, res) => {
     }catch(err){
         console.log(err)
         res.status(500).json({err: "failed to save info"})
+    }
+});
+
+//get all threads
+router.get("/thread", async(req, res) => {
+    try{
+        const threads = await Thread.find({}).sort({updatedAt: -1});
+        //in decending order
+        res.json(threads);
+
+    }catch(err){
+        console.log(err);
+        res.status[500].json({error: "failed to fetch threads"});
+    }
+});
+
+
+router.get("/thread/:threadId", async (req, res) => {
+    const {threadId} = req.params;
+    
+    try{
+     const thread = await Thread.findOne({threadId});
+
+     if(!thread){
+        res.status[404].json({error: "thread is not found"})
+     }
+     res.json(thread.messages);
+
+    }catch(err){
+        console.log(err);
+        res.status[500].json({error: "failed to fetch threads"});
+    }
+});
+
+router.delete("/thread/:threadId", async (req, res) => {
+    const {threadId} = req.params;
+
+    try{
+     const deleteThread = await Thread.findOneAndDelete({threadId});
+
+     if(!deleteThread){
+        res.status(404).json({error: "thread could not be deleted"});
+     }
+
+       res.status(200).json({success: "thread deleted succesfully"});
+
+    }catch(err){
+        console.log(err);
+        res.status[500].json({error: "failed to delete thread"});
     }
 });
 
