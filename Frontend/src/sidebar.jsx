6c278ab8cc390415241931523af2bb99e5e87682
om vazire <omvazire1195@gmail.com>
2026-06-05 20:15:36 +0530

    sidebar elements

--- Frontend/src/sidebar.jsx
+++ Frontend/src/sidebar.jsx
@@ -2,9 +2,28 @@ import "./sidebar.css";
 
 function Sidebar() {
   return (
-    <div>
-      sidebar
+    <section className="sidebar">
+      
+
+    <button>
+      <img src="src/assets/f1logo.png" alt="f1 logo" />
+      <i className="fa-solid fa-pen-to-square"></i>
+    </button>
+
+
+
+    <ul className="history">
+      <li>history1</li>
+      <li>history2</li>
+      <li>history3</li>
+    </ul>
+
+
+    <div className="sign">
+      <p>By Om_Vazire &hearts:</p>
     </div>
+
+    </section>
   );
 }
 
